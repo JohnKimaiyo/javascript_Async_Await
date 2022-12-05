f75e09820cc6fbe62511d164658ec0def1caea91
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-05 10:11:46 -0800

    asyn await called

---
+++ async 1 - Copy (15)/app.js
@@ -0,0 +1,9 @@
+async  function myDisplay(){
+    let myPromise = new Promise(function(resolve){
+        setTimeout(function(){resolve("react developer john")},3000);
+    });
+
+    console.log(await myPromise);
+}
+
+myDisplay();
